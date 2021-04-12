13af0d3c2d66892839feb08ba7c6caad94fec77d
Pre Muhu <pre.muhu@gmail.com>
2021-04-12 20:20:32 +0100

    build working

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
@@ -15,8 +16,14 @@ const todoList = require('./routes/todoList');
 const app = express();
 
 app.use(express.json());
-if(process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'development')app.use(morgan('dev'));
 app.use('/api/v1/todoList', todoList);
+
+if(process.env.NODE_ENV  === 'production'){
+	console.log('production');
+	app.use(express.static('my-app/build'));
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'my-app', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
